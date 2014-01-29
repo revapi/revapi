4746f0691a3480f44cf4f2c4bb61027e417ee576
Lukas Krejci <lkrejci@redhat.com>
2014-01-30 00:49:30 +0100

    Added "field added" check. No tests yet.

--- revapi-java/src/main/java/org/revapi/java/checks/fields/Added.java
+++ revapi-java/src/main/java/org/revapi/java/checks/fields/Added.java
@@ -16,10 +16,52 @@
 
 package org.revapi.java.checks.fields;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.VariableElement;
+
+import org.revapi.ChangeSeverity;
+import org.revapi.MatchReport;
+import org.revapi.java.checks.AbstractJavaCheck;
+import org.revapi.java.checks.Code;
+
 /**
  * @author Lukas Krejci
  * @since 0.1
  */
-public class Added {
-    //TODO implement
+public final class Added extends AbstractJavaCheck {
+
+    @Override
+    protected void doVisitField(VariableElement oldField, VariableElement newField) {
+        if (oldField == null && newField != null) {
+
+            boolean add = newField.getModifiers().contains(Modifier.PUBLIC) ||
+                newField.getModifiers().contains(Modifier.PROTECTED);
+
+            if (add) {
+                pushActive(null, newField);
+            }
+        }
+    }
+
+    @Override
+    protected List<MatchReport.Problem> doEnd() {
+        ActiveElements<VariableElement> fields = popIfActive();
+
+        if (fields == null) {
+            return null;
+        }
+
+        //do not bother with this on final classes - a new field on a final class cannot
+        //cause any problem
+        boolean fieldInFinalClass = fields.newElement.getEnclosingElement().getModifiers().contains(Modifier.FINAL);
+        ChangeSeverity binarySeverity = fieldInFinalClass ? null : ChangeSeverity.POTENTIALLY_BREAKING;
+        ChangeSeverity sourceSeverity =
+            fieldInFinalClass ? ChangeSeverity.NON_BREAKING : ChangeSeverity.POTENTIALLY_BREAKING;
+
+        return Collections
+            .singletonList(createProblem(Code.FIELD_ADDED, binarySeverity, sourceSeverity));
+    }
 }
