794e5f7a59dc20f150d3d8e8d1e35e50519ec492
Lukas Krejci <lkrejci@redhat.com>
2014-02-07 23:26:32 +0100

    Skeleton of method added check.

--- revapi-java/src/main/java/org/revapi/java/checks/methods/Added.java
+++ revapi-java/src/main/java/org/revapi/java/checks/methods/Added.java
@@ -16,9 +16,78 @@
 
 package org.revapi.java.checks.methods;
 
+import java.util.List;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.SimpleElementVisitor7;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.revapi.MatchReport;
+import org.revapi.java.checks.AbstractJavaCheck;
+
 /**
  * @author Lukas Krejci
  * @since 0.1
  */
-public class Added {
+public class Added extends AbstractJavaCheck {
+    private static final Logger LOG = LoggerFactory.getLogger(Added.class);
+
+    private final SimpleElementVisitor7<TypeElement, Void> enclosingClassExtractor = new SimpleElementVisitor7<TypeElement, Void>() {
+        @Override
+        protected TypeElement defaultAction(Element e, Void ignored) {
+            return null;
+        }
+
+        @Override
+        public TypeElement visitType(TypeElement e, Void ignored) {
+            return e;
+        }
+    };
+
+    @Override
+    protected void doVisitMethod(ExecutableElement oldMethod, ExecutableElement newMethod) {
+        if (oldMethod == null && newMethod != null) {
+            pushActive(null, newMethod);
+        }
+    }
+
+    @Override
+    protected List<MatchReport.Problem> doEnd() {
+        ActiveElements<ExecutableElement> methods = popIfActive();
+        if (methods == null) {
+            return null;
+        }
+
+        // we need to consider several cases here:
+        // 1) method added to a interface
+        // 2) method added to a final class
+        // 3) concrete method added to a non-final class
+        // 4) abstract method added to a non-final class
+
+        ExecutableElement method = methods.newElement;
+
+        TypeElement enclosingClass = method.accept(enclosingClassExtractor, null);
+        if (enclosingClass == null) {
+            LOG.warn("Could not find an enclosing class of method " + method + ". That's weird.");
+            return null;
+        }
+
+        if (enclosingClass.getKind() == ElementKind.INTERFACE) {
+            //TODO implement
+        } else if (enclosingClass.getModifiers().contains(Modifier.FINAL)) {
+            //TODO implement
+        } else if (method.getModifiers().contains(Modifier.ABSTRACT)) {
+            //TODO implement
+        } else {
+            //TODO implement
+        }
+
+        return null;
+    }
 }
