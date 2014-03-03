ff0027c02b326b0b4d8997d6deba2bc6093af749
Lukas Krejci <lkrejci@redhat.com>
2014-03-03 12:39:01 +0100

    Trivial - added suppress warnings..

--- revapi/src/main/java/org/revapi/Revapi.java
+++ revapi/src/main/java/org/revapi/Revapi.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014 Lukas Krejci
+ * Copyright $year Lukas Krejci
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -178,7 +178,7 @@ public final class Revapi {
         }
 
         @Nonnull
-        @SuppressWarnings("unchecked")
+        @SuppressWarnings({"unchecked", "rawtypes"})
         public Builder withConfiguration(@Nonnull Properties properties) {
             this.configuration = (Map<String, String>) (Map) properties;
             return this;
