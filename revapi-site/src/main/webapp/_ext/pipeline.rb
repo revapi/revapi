7913961668f5ef63069dc15828e7fefba376989e
Lukas Krejci <lkrejci@redhat.com>
2014-03-06 03:31:15 +0100

    Adding an awestruct site.

---
+++ revapi-site/src/main/webapp/_ext/pipeline.rb
@@ -0,0 +1,10 @@
+require 'docs'
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Docs::Index.new('/docs', :docs)
+
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+end
