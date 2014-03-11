1e360adfce95cfd71fab1d1240cc038c7c3d4ec5
Lukas Krejci <lkrejci@redhat.com>
2014-03-11 15:15:32 +0100

    trivial - rename the text reporter's config property.

--- revapi-reporting-text/src/main/java/org/revapi/TextReporter.java
+++ revapi-reporting-text/src/main/java/org/revapi/TextReporter.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -43,7 +43,7 @@ public class TextReporter implements Reporter {
 
     @Override
     public void initialize(@Nonnull Configuration config) {
-        String minLevel = config.getProperties().get("revapi.reporter.text.minLevel");
+        String minLevel = config.getProperties().get("revapi.reporter.text.minSeverity");
         String output = config.getProperties().get("revapi.reporter.text.output");
         output = output == null ? "out" : output;
 
