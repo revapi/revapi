d4cc5652a734da2f72b55139dc9b0ed9b49ee26b
Lukas Krejci <lkrejci@redhat.com>
2014-03-11 15:16:18 +0100

    trivial - correct usage help for standalone.

--- revapi-standalone/src/main/java/org/revapi/standalone/Main.java
+++ revapi-standalone/src/main/java/org/revapi/standalone/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Lukas Krejci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
 package org.revapi.standalone;
 
 import java.io.File;
@@ -46,7 +62,7 @@ public final class Main {
         }
 
         System.out.println(progName +
-            " [-u|-h] -e <GAV>[,<GAV>]* -o <FILE>[,<FILE>]* -n <FILE>[,<FILE>]* [-s <FILE>[:<FILE>]*] [-t <FILE>[:<FILE>]*] [-D<CONFIG_OPTION>=<VALUE>]* [-c <FILE>[,<FILE>]*] [-r <DIR>]");
+            " [-u|-h] -e <GAV>[,<GAV>]* -o <FILE>[,<FILE>]* -n <FILE>[,<FILE>]* [-s <FILE>[,<FILE>]*] [-t <FILE>[,<FILE>]*] [-D<CONFIG_OPTION>=<VALUE>]* [-c <FILE>[,<FILE>]*] [-r <DIR>]");
         System.out.println();
         System.out.println(pad + " -u");
         System.out.println(pad + " -h");
