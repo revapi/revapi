ead49bdb312d27c3ba9676c526246c40cdb1050c
Lukas Krejci <krejci.l@centrum.cz>
2014-11-27 22:57:14 +0100

    Added missing GoogleAnalytics helper to awestruct pipeline

--- src/site/_ext/pipeline.rb
+++ src/site/_ext/pipeline.rb
@@ -3,9 +3,5 @@ require 'coderay'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Docs::Index.new('/docs', :docs)
-
-  # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
-  # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  helper Awestruct::Extensions::GoogleAnalytics
 end
