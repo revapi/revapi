2eaf1e93f03be7a7896c619ab9e2896b0da997c9
Lukas Krejci <krejci.l@centrum.cz>
2016-03-29 22:17:56 +0200

    Change the text reporter artifactId to correspond with its package and config names.

--- revapi-reporter-text/src/main/java/org/revapi/reporter/text/TextReporter.java
+++ revapi-reporter-text/src/main/java/org/revapi/reporter/text/TextReporter.java
@@ -43,7 +43,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import freemarker.cache.ClassTemplateLoader;
-import freemarker.cache.FileTemplateLoader;
 import freemarker.cache.MultiTemplateLoader;
 import freemarker.cache.TemplateLoader;
 import freemarker.template.Configuration;
