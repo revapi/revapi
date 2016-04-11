revapi/revapi

commit dd3e165d79758af898136c4554efa96f9deb8276

[#35] Move class and package filter right into API tree construction logic.
This is necessary so that we can be clever and correctly not drag the types
that are used by the ignored classes into the API. That would be the case
if the filtering was done at the filter stage (as it was done originally)
because the API tree would already be constructed and the "drags"
determined. It would then be much harder/more expensive to figure out if a
class is only dragged into API by an ignored type.

revapi-java/src/main/java/org/revapi/java/compilation/TypeTreeConstructor.java
@@ -17,6 +17,7 @@
 package org.revapi.java.compilation;
 
 import java.io.File;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -50,10 +51,19 @@ final class TypeTreeConstructor {
     private final ProbingEnvironment environment;
     private final Set<File> bootstrapClasspath;
     private Set<String> bootstrapClasses;
+    private final InclusionFilter inclusionFilter;
 
-    TypeTreeConstructor(ProbingEnvironment environment, Set<File> bootstrapClasspath) {
+    /**
+     * @param environment the type environment we're operating in
+     * @param bootstrapClasspath the jars of the bootstrap classpath
+     * @param inclusionFilter a filter on canonical class name deciding whether to include the class in the API or not
+     *                        This is meant to come from user configuration.
+     */
+    TypeTreeConstructor(ProbingEnvironment environment, Set<File> bootstrapClasspath,
+            InclusionFilter inclusionFilter) {
         this.environment = environment;
         this.bootstrapClasspath = bootstrapClasspath;
+        this.inclusionFilter = inclusionFilter;
     }
 
     public ClassProcessor createApiClassProcessor(Archive classArchive, String classBinaryName, boolean apiType) {
@@ -80,6 +90,15 @@ final class TypeTreeConstructor {
 
         for (TypeRecord t : allTypes) {
             if (t.isApiType()) {
+                if (t.isExplicitlyExcluded()) {
+                    environment.addExplicitExclusion(t.getType().getCanonicalName());
+                    continue;
+                }
+
+                if (!inclusionFilter.defaultCase() && !t.isExplicitlyIncluded()) {
+                    continue;
+                }
+
                 if (t.getType() == null) {
                     unknownTypes.add(t.getBinaryName());
                     continue;
@@ -92,8 +111,23 @@ final class TypeTreeConstructor {
                 //before their children.
                 if (t.getOwner() == null) {
                     environment.getTree().getRootsUnsafe().add(t.getType());
-                } else if (t.isApiThroughUse() && checkInTree(t.getOwner()) == null) {
-                    environment.getTree().getRootsUnsafe().add(t.getType());
+                } else if (checkInTree(t.getOwner()) == null) {
+                    boolean include = true;
+                    if (!t.isExplicitlyIncluded()) {
+                        //check if there was an explicit exclusion of one of the parent
+                        TypeRecord parent = t.getOwner();
+                        while (parent != null) {
+                            if (parent.isExplicitlyExcluded()) {
+                                include = false;
+                                break;
+                            }
+                            parent = parent.getOwner();
+                        }
+                    }
+
+                    if (include) {
+                        environment.getTree().getRootsUnsafe().add(t.getType());
+                    }
                 }
 
                 if (t.hasUseSites()) {
@@ -230,6 +264,7 @@ final class TypeTreeConstructor {
         private final String classBinaryName;
         private final boolean apiType;
         private InnerClassHierarchyConstructor innerClassHierarchyConstructor;
+        private final List<AbstractMap.SimpleEntry<String, RawUseSite>> detectedUses = new ArrayList<>();
 
         private ClassProcessor(Archive archive, String classBinaryName, boolean apiType) {
             this.archive = archive;
@@ -246,6 +281,13 @@ final class TypeTreeConstructor {
         }
 
         public void addUse(String usedTypeBinaryName, RawUseSite useSite) {
+            detectedUses.add(new AbstractMap.SimpleEntry<>(usedTypeBinaryName, useSite));
+        }
+
+        private void processUse(Map.Entry<String, RawUseSite> usedTypeAndUseSite) {
+            String usedTypeBinaryName = usedTypeAndUseSite.getKey();
+            RawUseSite useSite = usedTypeAndUseSite.getValue();
+
             if (isOnBootstrapClasspath(usedTypeBinaryName)) {
                 return;
             }
@@ -284,6 +326,23 @@ final class TypeTreeConstructor {
             TypeRecord rec = getOrCreateTypeRecord(classBinaryName);
             unseenClassesBinaryNames.remove(classBinaryName);
 
+            List<InnerClass> innerClassHierarchy = null;
+            if (innerClassHierarchyConstructor != null) {
+                innerClassHierarchy = innerClassHierarchyConstructor.process();
+            }
+
+            String canonicalName = innerClassHierarchyConstructor == null
+                    ? classBinaryName
+                    : innerClassHierarchy.get(innerClassHierarchy.size() - 1).getCanonicalName();
+
+            if (inclusionFilter.rejects(classBinaryName, canonicalName)) {
+                rec.setExplicitlyExcluded(true);
+            } else {
+                detectedUses.forEach(this::processUse);
+            }
+
+            rec.setExplicitlyIncluded(inclusionFilter.accepts(classBinaryName, canonicalName));
+
             //if this was determined as part of API, don't reset it back (potentially)
             rec.setApiType(rec.isApiType() || apiType);
 
@@ -295,7 +354,7 @@ final class TypeTreeConstructor {
                 return;
             }
 
-            if (innerClassHierarchyConstructor == null) {
+            if (innerClassHierarchy == null) {
                 TypeElement type = createTypeElement(archive, classBinaryName, classBinaryName);
                 rec.setType(type);
                 initChildren(rec, 1);
@@ -304,7 +363,6 @@ final class TypeTreeConstructor {
                 //for the current class, we already did it above, but we don't
                 //do anything for the parents - their apiType flag will be set
                 //when their classes are processed.
-                List<InnerClass> innerClassHierarchy = innerClassHierarchyConstructor.process();
                 if (innerClassHierarchy.isEmpty()) {
                     //anonymous inner class most probably
                     return;

revapi-java-spi/pom.xml
@@ -31,7 +31,7 @@
     <url>${web.url}/modules/${project.artifactId}</url>
     
     <artifactId>revapi-java-spi</artifactId>
-    <version>0.6.1-SNAPSHOT</version>
+    <version>0.7.0-SNAPSHOT</version>
 
     <dependencies>
         <dependency>

revapi-java-spi/src/main/java/org/revapi/java/spi/CheckBase.java
@@ -362,6 +362,7 @@ public abstract class CheckBase implements Check {
     private TypeEnvironment oldTypeEnvironment;
     private TypeEnvironment newTypeEnvironment;
     private int depth;
+    private boolean dummyRun;
     private final Deque<ActiveElements<?>> activations = new ArrayDeque<>();
     private AnalysisContext analysisContext;
 
@@ -453,7 +454,7 @@ public abstract class CheckBase implements Check {
     @Override
     public final List<Difference> visitEnd() {
         try {
-            return doEnd();
+            return dummyRun ? null : doEnd();
         } finally {
             //defensive pop if the doEnd "forgets" to do it.
             //this is to prevent accidental retrieval of wrong data in the case the last active element was pushed
@@ -461,6 +462,7 @@ public abstract class CheckBase implements Check {
             //even if the visit call didn't push anything to the stack.
             popIfActive();
             depth--;
+            dummyRun = false;
         }
     }
 
@@ -478,7 +480,11 @@ public abstract class CheckBase implements Check {
     @Override
     public final void visitClass(@Nullable TypeElement oldType, @Nullable TypeElement newType) {
         depth++;
-        doVisitClass(oldType, newType);
+        if (isPotentiallyInApi(oldType, newType)) {
+            doVisitClass(oldType, newType);
+        } else {
+            dummyRun = true;
+        }
     }
 
     protected void doVisitClass(@Nullable TypeElement oldType, @Nullable TypeElement newType) {
@@ -494,7 +500,11 @@ public abstract class CheckBase implements Check {
     @Override
     public final void visitMethod(@Nullable ExecutableElement oldMethod, @Nullable ExecutableElement newMethod) {
         depth++;
-        doVisitMethod(oldMethod, newMethod);
+        if (isPotentiallyInApi(oldMethod, newMethod)) {
+            doVisitMethod(oldMethod, newMethod);
+        } else {
+            dummyRun = true;
+        }
     }
 
     protected void doVisitMethod(@Nullable ExecutableElement oldMethod, @Nullable ExecutableElement newMethod) {
@@ -504,7 +514,11 @@ public abstract class CheckBase implements Check {
     public final void visitMethodParameter(@Nullable VariableElement oldParameter,
         @Nullable VariableElement newParameter) {
         depth++;
-        doVisitMethodParameter(oldParameter, newParameter);
+        if (isPotentiallyInApi(oldParameter, newParameter)) {
+            doVisitMethodParameter(oldParameter, newParameter);
+        } else {
+            dummyRun = true;
+        }
     }
 
     @SuppressWarnings("UnusedParameters")
@@ -522,7 +536,11 @@ public abstract class CheckBase implements Check {
     @Override
     public final void visitField(@Nullable VariableElement oldField, @Nullable VariableElement newField) {
         depth++;
-        doVisitField(oldField, newField);
+        if (isPotentiallyInApi(oldField, newField)) {
+            doVisitField(oldField, newField);
+        } else {
+            dummyRun = true;
+        }
     }
 
     protected void doVisitField(@Nullable VariableElement oldField, @Nullable VariableElement newField) {
@@ -586,5 +604,9 @@ public abstract class CheckBase implements Check {
     protected <T extends Element> ActiveElements<T> popIfActive() {
         return (ActiveElements<T>) (!activations.isEmpty() && activations.peek().depth == depth ? activations.pop() :
             null);
+    }
+
+    private boolean isPotentiallyInApi(Element oldEl, Element newEl) {
+        return !(oldTypeEnvironment.isExplicitlyExcluded(oldEl) && newTypeEnvironment.isExplicitlyExcluded(newEl));
     }
 }

revapi-java-spi/src/main/java/org/revapi/java/spi/TypeEnvironment.java
@@ -20,6 +20,7 @@ import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
@@ -71,4 +72,30 @@ public interface TypeEnvironment {
      */
     @Nonnull
     Set<TypeElement> getAccessibleSubclasses(@Nonnull TypeElement type);
+
+    /**
+     * This returns true for elements that are included by the means of configuration. I.e. even though they could
+     * otherwise be excluded from the API, the user chose to include them.
+     * <p/>
+     * Note that this is not a mere opposite of {@link #isExplicitlyExcluded(Element)}. Both methods can return false
+     * for a single element, which means that the inclusion state of that element is implicit. Usually this means that
+     * the inclusion is dependent on the parent element.
+     *
+     * @param element the element to check
+     * @return true if this element is explicitly included by configuration, false otherwise
+     */
+    boolean isExplicitlyIncluded(Element element);
+
+    /**
+     * This returns true for elements that are excluded by the means of configuration. I.e. even though they would
+     * otherwise be included in the API, they are excluded by the user.
+     * <p/>
+     * It does not mean that the element is to be included in the API checks if this method returns false. That merely
+     * means that the user didn't explicitly exclude it and further checks need to be made to establish whether to check
+     * the element or not (see for example {@link CheckBase#isAccessible(Element, TypeEnvironment)}).
+     *
+     * @param element the element to check (might be type, method, whatever)
+     * @return true if the the user explicitly excluded this element from the API checks, false otherwise.
+     */
+    boolean isExplicitlyExcluded(Element element);
 }

revapi-java-spi/src/test/java/org/revapi/java/test/TestTypeEnvironment.java
@@ -91,6 +91,16 @@ public class TestTypeEnvironment implements TypeEnvironment {
         return Collections.emptySet();
     }
 
+    @Override
+    public boolean isExplicitlyIncluded(Element element) {
+        return true;
+    }
+
+    @Override
+    public boolean isExplicitlyExcluded(Element element) {
+        return false;
+    }
+
     public static class Builder {
         private Elements elements;
         private Types types;

revapi-java/pom.xml
@@ -31,7 +31,7 @@
     <url>${web.url}/modules/${project.artifactId}</url>
     
     <artifactId>revapi-java</artifactId>
-    <version>0.8.1-SNAPSHOT</version>
+    <version>0.9.0-SNAPSHOT</version>
 
     <dependencyManagement>
         <dependencies>
@@ -54,6 +54,7 @@
         <dependency>
             <groupId>org.revapi</groupId>
             <artifactId>revapi-java-spi</artifactId>
+            <version>0.7.0-SNAPSHOT</version>
         </dependency>
 
         <dependency>

revapi-java/src/main/java/org/revapi/java/AnalysisConfiguration.java
@@ -17,8 +17,11 @@
 package org.revapi.java;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.jboss.dmr.ModelNode;
 
@@ -34,17 +37,25 @@ public final class AnalysisConfiguration {
     private final Set<File> newApiBootstrapClasspath;
     private final boolean ignoreMissingAnnotations;
     private final boolean deepUseChainAnalysis;
+    private final Set<Pattern> classInclusionFilters;
+    private final Set<Pattern> classExclusionFilters;
+    private final Set<Pattern> packageInclusionFilters;
+    private final Set<Pattern> packageExclusionFilters;
 
-    public AnalysisConfiguration(MissingClassReporting missingClassReporting,
-        Set<String> useReportingCodes, Set<File> oldApiBootstrapClasspath,
-        Set<File> newApiBootstrapClasspath, boolean ignoreMissingAnnotations, boolean deepUseChainAnalysis) {
-
+    public AnalysisConfiguration(MissingClassReporting missingClassReporting, Set<String> useReportingCodes,
+            Set<File> oldApiBootstrapClasspath, Set<File> newApiBootstrapClasspath, boolean ignoreMissingAnnotations,
+            boolean deepUseChainAnalysis, Set<Pattern> classInclusionFilters, Set<Pattern> classExclusionFilters,
+            Set<Pattern> packageInclusionFilters, Set<Pattern> packageExclusionFilters) {
         this.missingClassReporting = missingClassReporting;
         this.useReportingCodes = useReportingCodes;
         this.oldApiBootstrapClasspath = oldApiBootstrapClasspath;
         this.newApiBootstrapClasspath = newApiBootstrapClasspath;
         this.ignoreMissingAnnotations = ignoreMissingAnnotations;
         this.deepUseChainAnalysis = deepUseChainAnalysis;
+        this.classInclusionFilters = classInclusionFilters;
+        this.classExclusionFilters = classExclusionFilters;
+        this.packageInclusionFilters = packageInclusionFilters;
+        this.packageExclusionFilters = packageExclusionFilters;
     }
 
     public static AnalysisConfiguration fromModel(ModelNode node) {
@@ -55,8 +66,21 @@ public final class AnalysisConfiguration {
         boolean ignoreMissingAnnotations = readIgnoreMissingAnnotations(node);
         boolean deepUseChainAnalysis = readDeepUseChainAnalysis(node);
 
+        ModelNode classesRegex = node.get("revapi", "java", "filter", "classes", "regex");
+        ModelNode packagesRegex = node.get("revapi", "java", "filter", "packages", "regex");
+
+        Set<Pattern> classInclusionFilters = readFilter(node.get("revapi", "java", "filter", "classes", "include"),
+                classesRegex);
+        Set<Pattern> classExclusionFilters = readFilter(node.get("revapi", "java", "filter", "classes", "exclude"),
+                classesRegex);
+        Set<Pattern> packageInclusionFilters = readFilter(node.get("revapi", "java", "filter", "packages", "include"),
+                packagesRegex);
+        Set<Pattern> packageExclusionFilters = readFilter(node.get("revapi", "java", "filter", "packages", "exclude"),
+                packagesRegex);
+
         return new AnalysisConfiguration(reporting, useReportingCodes, oldApiBootstrapClasspath,
-            newApiBootstrapClasspath, ignoreMissingAnnotations, deepUseChainAnalysis);
+            newApiBootstrapClasspath, ignoreMissingAnnotations, deepUseChainAnalysis, classInclusionFilters,
+                classExclusionFilters, packageInclusionFilters, packageExclusionFilters);
     }
 
     public MissingClassReporting getMissingClassReporting() {
@@ -81,6 +105,22 @@ public final class AnalysisConfiguration {
 
     public boolean isDeepUseChainAnalysis() {
         return deepUseChainAnalysis;
+    }
+
+    public Set<Pattern> getClassExclusionFilters() {
+        return classExclusionFilters;
+    }
+
+    public Set<Pattern> getClassInclusionFilters() {
+        return classInclusionFilters;
+    }
+
+    public Set<Pattern> getPackageExclusionFilters() {
+        return packageExclusionFilters;
+    }
+
+    public Set<Pattern> getPackageInclusionFilters() {
+        return packageInclusionFilters;
     }
 
     private static MissingClassReporting readMissingClassReporting(ModelNode analysisConfig) {
@@ -187,6 +227,24 @@ public final class AnalysisConfiguration {
     private static boolean readDeepUseChainAnalysis(ModelNode analysisConfig) {
         ModelNode config = analysisConfig.get("revapi", "java", "deepUseChainAnalysis");
         return config.isDefined() && config.asBoolean();
+    }
+
+    private static Set<Pattern> readFilter(ModelNode filterNode, ModelNode regexNode) {
+        if (!filterNode.isDefined()) {
+            return Collections.emptySet();
+        }
+
+        boolean isRegex = regexNode.isDefined() && regexNode.asBoolean();
+
+        return filterNode.asList().stream()
+                .map(filter -> {
+                    if (isRegex) {
+                        return Pattern.compile(filter.asString());
+                    } else {
+                        return Pattern.compile(Pattern.quote(filter.asString()));
+                    }
+                })
+                .collect(Collectors.toSet());
     }
 
     public enum MissingClassReporting {

revapi-java/src/main/java/org/revapi/java/JavaApiAnalyzer.java
@@ -37,6 +37,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.BiFunction;
+import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -51,6 +52,7 @@ import org.revapi.CorrespondenceComparatorDeducer;
 import org.revapi.DifferenceAnalyzer;
 import org.revapi.Element;
 import org.revapi.java.compilation.CompilationValve;
+import org.revapi.java.compilation.InclusionFilter;
 import org.revapi.java.compilation.ProbingEnvironment;
 import org.revapi.java.model.JavaElementFactory;
 import org.revapi.java.model.MethodElement;
@@ -429,9 +431,10 @@ public final class JavaApiAnalyzer implements ApiAnalyzer {
                         configuration.getNewApiBootstrapClasspath();
         boolean ignoreMissingAnnotations = configuration.isIgnoreMissingAnnotations();
         boolean skipUseTracking = !configuration.isDeepUseChainAnalysis();
+        InclusionFilter inclusionFilter = composeInclusionFilter(configuration);
 
         return new JavaArchiveAnalyzer(api, compilationExecutor, configuration.getMissingClassReporting(),
-                ignoreMissingAnnotations, skipUseTracking, bootstrapClasspath);
+                ignoreMissingAnnotations, skipUseTracking, bootstrapClasspath, inclusionFilter);
     }
 
     @Nonnull
@@ -453,5 +456,60 @@ public final class JavaApiAnalyzer implements ApiAnalyzer {
     @Override
     public void close() {
         compilationExecutor.shutdown();
+    }
+
+    private static InclusionFilter composeInclusionFilter(AnalysisConfiguration config) {
+        final Set<Pattern> inclClasses = config.getClassInclusionFilters();
+        final Set<Pattern> exclClasses = config.getClassExclusionFilters();
+        final Set<Pattern> inclPkgs = config.getPackageInclusionFilters();
+        final Set<Pattern> exclPkgs = config.getPackageExclusionFilters();
+
+        return new InclusionFilter() {
+            @Override
+            public boolean accepts(String typeBinaryName, String typeCanonicalName) {
+                for (Pattern p : inclClasses) {
+                    if (p.matcher(typeCanonicalName).matches()) {
+                        return true;
+                    }
+                }
+
+                int lastDot = typeBinaryName.lastIndexOf('.');
+                String pkg = lastDot == -1 ? "" : typeBinaryName.substring(0, lastDot);
+
+
+                for (Pattern p : inclPkgs) {
+                    if (p.matcher(pkg).matches()) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean rejects(String typeBinaryName, String typeCanonicalName) {
+                for (Pattern p : exclClasses) {
+                    if (p.matcher(typeCanonicalName).matches()) {
+                        return true;
+                    }
+                }
+
+                int lastDot = typeBinaryName.lastIndexOf('.');
+                String pkg = lastDot == -1 ? "" : typeBinaryName.substring(0, lastDot);
+
+                for (Pattern p : exclPkgs) {
+                    if (p.matcher(pkg).matches()) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean defaultCase() {
+                return inclClasses.isEmpty() && inclPkgs.isEmpty();
+            }
+        };
     }
 }

commit dd7df0ebcf4d32f7fa55ee6539318423b1510e20

Fix the regression introduced by dd3e165d79758af898136c4554efa96f9deb8276.
Don't fall over on determining the canonical class name of an anonymous
or member class (which don't have one).

revapi-java/src/main/java/org/revapi/java/compilation/TypeTreeConstructor.java
@@ -331,9 +331,16 @@ final class TypeTreeConstructor {
                 innerClassHierarchy = innerClassHierarchyConstructor.process();
             }
 
-            String canonicalName = innerClassHierarchyConstructor == null
-                    ? classBinaryName
-                    : innerClassHierarchy.get(innerClassHierarchy.size() - 1).getCanonicalName();
+            String canonicalName;
+
+            if (innerClassHierarchyConstructor == null) {
+                canonicalName = classBinaryName;
+            } else if (!innerClassHierarchy.isEmpty()) {
+                canonicalName = innerClassHierarchy.get(innerClassHierarchy.size() - 1).getCanonicalName();
+            } else {
+                //anonymous inner class or a member class.. we don't consider those.
+                return;
+            }
 
             if (inclusionFilter.rejects(classBinaryName, canonicalName)) {
                 rec.setExplicitlyExcluded(true);