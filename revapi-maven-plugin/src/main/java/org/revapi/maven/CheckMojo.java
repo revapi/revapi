33a7ba45c4b64c11633db36455f9fc49b26b185c
Lukas Krejci <lukas@krejci.pw>
2018-04-26 15:19:45 +0200

    Pass a writer and the maven log as contextual data to the extensions so that they can contribute to the output during the maven build.

--- revapi-maven-plugin/src/main/java/org/revapi/maven/CheckMojo.java
+++ revapi-maven-plugin/src/main/java/org/revapi/maven/CheckMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Lukas Krejci
+ * Copyright 2014-2018 Lukas Krejci
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,6 +23,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.revapi.AnalysisResult;
 
+import java.io.StringWriter;
+
 /**
  * Runs the API check of old and new artifacts using the specified configuration of extensions declared as dependencies
  * of the plugin.
@@ -40,27 +42,36 @@ public class CheckMojo extends AbstractRevapiMojo {
             return;
         }
 
+        StringWriter wrt = new StringWriter();
+        BuildTimeReporter reporter;
+
         try (AnalysisResult res = analyze(BuildTimeReporter.class,
-                BuildTimeReporter.BREAKING_SEVERITY_KEY, failSeverity.asDifferenceSeverity())) {
+                BuildTimeReporter.BREAKING_SEVERITY_KEY, failSeverity.asDifferenceSeverity(), "maven-log", getLog(),
+                "writer", wrt)) {
 
             res.throwIfFailed();
 
-            BuildTimeReporter reporter = res.getExtensions().getFirstExtension(BuildTimeReporter.class, null);
-
-            if (reporter != null && reporter.hasBreakingProblems()) {
-                if (failBuildOnProblemsFound) {
-                    throw new MojoFailureException(reporter.getAllProblemsMessage());
-                } else {
-                    getLog().info("API problems found but letting the build pass as configured.");
-                }
-            } else {
-                getLog().info("API checks completed without failures.");
-            }
-
+            reporter = res.getExtensions().getFirstExtension(BuildTimeReporter.class, null);
         } catch (MojoFailureException e) {
             throw e;
         } catch (Exception e) {
             throw new MojoExecutionException("Failed to execute the API analysis.", e);
+        }
+
+        if (reporter != null && reporter.hasBreakingProblems()) {
+            if (failBuildOnProblemsFound) {
+                String report = reporter.getAllProblemsMessage();
+                String additionalOutput = wrt.toString();
+                if (!additionalOutput.isEmpty()) {
+                    report += "\n\nAdditionally, the configured reporters reported:\n\n" + additionalOutput;
+                }
+
+                throw new MojoFailureException(report);
+            } else {
+                getLog().info("API problems found but letting the build pass as configured.");
+            }
+        } else {
+            getLog().info("API checks completed without failures.");
         }
     }
 }
