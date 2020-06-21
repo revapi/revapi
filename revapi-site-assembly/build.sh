c141960ed24dab867d75f81476f0972fa9ce0e65
Lukas Krejci <lukas@krejci.pw>
2020-06-21 23:41:24 +0200

    wip

--- revapi-site-assembly/build.sh
+++ revapi-site-assembly/build.sh
@@ -3,3 +3,6 @@
 npm install @antora/cli @antora/site-generator-default rss asciidoctor-plantuml
 
 node_modules/@antora/cli/bin/antora $@
+
+# a hack before Antora solves https://gitlab.com/antora/antora/-/issues/627
+touch build/site/.nojekyll
