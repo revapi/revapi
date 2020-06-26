685422881e3ac6573f7b71886c80957153810ba6
Lukas Krejci <lukas@krejci.pw>
2020-06-26 02:36:46 +0200

    Add lunr search to the site.

--- revapi-site-assembly/build.sh
+++ revapi-site-assembly/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-npm install @antora/cli @antora/site-generator-default rss asciidoctor-plantuml
+npm install @antora/cli @antora/site-generator-default rss asciidoctor-plantuml antora-site-generator-lunr
 
-node_modules/@antora/cli/bin/antora $@
+DOCSEARCH_ENABLED=true \
+DOCSEARCH_ENGINE=lunr \
+DOCSEARCH_INDEX_VERSION=latest \
+NODE_PATH="$(npm -g root)" \
+node_modules/@antora/cli/bin/antora --generator antora-site-generator-lunr $@
 
 # a hack before Antora solves https://gitlab.com/antora/antora/-/issues/627
 touch build/site/.nojekyll
