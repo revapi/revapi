ff846de707cd8108aedbc15ccbe73df7b69c3a78
Lukas Krejci <lukas@krejci.pw>
2020-07-29 10:40:42 +0200

    Add a confirmation question before the release start so that all assumptions about the release are met.

--- release.sh
+++ release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "The site should be published only when there are no unreleased changes to the docs of the individual modules.
+I.e. make sure that you release all the modules that have docs changes."
+read -p "Are you sure you want to start the release and site publication process? [y/n]: " start_release
+if [ "$start_release" != "y" ] && [ "$start_release" != "Y" ]; then
+  echo "Aborting."
+  exit 1
+fi
+
 ALL_MODULES=" $(ls -df1 revapi-* | sed 's/-/_/g') revapi"
 
 DEPS_revapi_parent=()
