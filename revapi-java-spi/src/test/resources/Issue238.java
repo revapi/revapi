d7c10d825a9ca12e5f79f899e5c3b2a2de82cc21
Daniel Hooker <daniel.hooker.at@gmail.com>
2021-06-11 09:05:14 +0200

    changed hierarchy

--- revapi-java-spi/src/test/resources/Issue238.java
+++ revapi-java-spi/src/test/resources/Issue238.java
@@ -1,20 +1,26 @@
 import java.io.Serializable;
 
 public class Issue238 {
-    public interface ValueSet extends Serializable {}
-    public interface Range<T extends Comparable<? super T>> extends ValueSet {
+    public interface ValueSet<T> extends Serializable {
         boolean contains(T value);
     }
+    public interface Range<T extends Comparable<? super T>> extends ValueSet<T> {}
     public class DefaultRange<T extends Comparable<? super T>> implements Range<T> {
         @Override
         public boolean contains(T value) {
             return false;
         }
     }
-    public class MoneyRange<T extends Comparable<? super T>> extends DefaultRange<T> {
+    public class MoneyRange extends DefaultRange<Money> {
         @Override
-        public boolean contains(T value) {
+        public boolean contains(Money value) {
             return false;
+        }
+    }
+    public class Money implements Comparable<Money>, Serializable {
+        @Override
+        public int compareTo(Money other) {
+            return 1;
         }
     }
 }
