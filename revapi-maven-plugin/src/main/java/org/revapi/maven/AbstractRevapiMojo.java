c920e81df70587466e8775f8a56ab582222e6118
Matthias Stock <mail@mstock.de>
2021-06-19 01:17:01 +0200

    Minor improvements to the documentation
    - fixed broken apidocs and architecture links
    - minor spelling corrections
    - removed unnecessary (and not rendered) HTML encoding from `{@code }` JavaDoc markup
    - using https in READMEs where possible
    - using https in Antora adoc where possible (although it seems to be changed automatically)

--- revapi-maven-plugin/src/main/java/org/revapi/maven/AbstractRevapiMojo.java
+++ revapi-maven-plugin/src/main/java/org/revapi/maven/AbstractRevapiMojo.java
@@ -54,7 +54,7 @@ abstract class AbstractRevapiMojo extends AbstractMojo {
 
     /**
      * The JSON or XML configuration of various analysis options. The available options depend on what analyzers are
-     * present on the plugin classpath through the {@code &lt;dependencies&gt;}. Consult
+     * present on the plugin classpath through the {@code <dependencies>}. Consult
      * <a href="examples/configuration.html">configuration documentation</a> for more details.
      *
      * <p>
@@ -75,7 +75,7 @@ abstract class AbstractRevapiMojo extends AbstractMojo {
 
     /**
      * The list of files containing the configuration of various analysis options. The available options depend on what
-     * analyzers are present on the plugins classpath through the {@code &lt;dependencies&gt;}.
+     * analyzers are present on the plugins classpath through the {@code <dependencies>}.
      *
      * <p>
      * The {@code analysisConfiguration} can override the settings present in the files.
