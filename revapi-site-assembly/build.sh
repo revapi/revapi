07438ec3863b98ac2ab3e381f9fc53c901eb5364
Lukas Krejci <lukas@krejci.pw>
2025-04-24 15:56:27 +0200

    run kroki locally and generate diagrams at site build time so that we're not dependent on an external service for them

--- revapi-site-assembly/build.sh
+++ revapi-site-assembly/build.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+DOCKER=podman
+
+if ! which $DOCKER >/dev/null 2>&1; then
+  DOCKER=docker
+fi
+
+if ! which $DOCKER >/dev/null 2>&1; then
+  echo "podman or docker required"
+  exit 1
+fi
+
+$DOCKER run -p8000:8000 --name local-kroki-installation-for-revapi-build -d docker.io/yuzutech/kroki
+
 npm install @antora/cli @antora/site-generator-default rss asciidoctor-kroki antora-site-generator-lunr
 
 DOCSEARCH_ENABLED=true \
-DOCSEARCH_ENGINE=lunr \
-DOCSEARCH_INDEX_VERSION=latest \
-NODE_PATH="$(npm -g root)" \
-node_modules/@antora/cli/bin/antora --generator antora-site-generator-lunr $@
+  DOCSEARCH_ENGINE=lunr \
+  DOCSEARCH_INDEX_VERSION=latest \
+  NODE_PATH="$(npm -g root)" \
+  node_modules/@antora/cli/bin/antora --generator antora-site-generator-lunr $@
+
+$DOCKER kill local-kroki-installation-for-revapi-build
